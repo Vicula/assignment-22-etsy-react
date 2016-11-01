61e41cc5c7c783611d2db518b4ff6c25e5705223
Victor Carpenter <victorcarpenter123@gmail.com>
2016-11-01 17:20:37 -0400

    slowly recreating the site

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -22,25 +22,39 @@ const $ = require('jquery')
 
 
 
-var homePage = React.createClass({
+var HomePage = React.createClass({
    render: function(){
+
+      let jumboTitleText = "Whoever you are, find whatever you're into"
 
 
       return (
          <div>
-            <h1>Workings</h1>
-            // <nav>
-            //    <img src="../images/etsy-logo.png" className="navLogo"/>
-            //    <div className="navIcons">
-            //       <span className="glyphicon glyphicon-home"></span>
-            //       <span className="glyphicon glyphicon-bell"></span>
-            //       <span className="glyphicon glyphicon-shopping-cart"></span>
-            //    </div>
-            // </nav>
-         </div>
+            <nav>
+               <img src="../images/etsy-logo.png" className="navLogo"/>
+               <div className="navIcons">
+                  <img src="../images/Home-48.png"/>
+                  <img src="../images/Appointment Reminders-48.png"/>
+                  <img src="../images/Shopping Cart-48.png"/>
+               </div>
+            </nav>
+            <div>
+               <div className="jumbotron">
+                  <h1 className="jumboTitle">{jumboTitleText}</h1>
+                  <div className="input-group jumboSearch">
+                     <input type="text" className="form-control jumboSearchBar"/>
+                     <div className="input-group-btn">
+                        <button type="button" className="btn default jumboSearchBut">Search</button>
+                     </div>
+                  </div>
+               </div>
+               <div className="homeInfoBlocksbox">
 
+               </div>
+            </div>
+         </div>
       )
    }
 })
 
-ReactDOM.render(<homePage/>, document.querySelector('#app-container'))
+ReactDOM.render(<HomePage/>, document.querySelector('#app-container'))
